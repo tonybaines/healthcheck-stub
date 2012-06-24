4caa201b8c14ddff9a20cb52294b790e4a080067
Tony Baines <tony.baines.1@gmail.com>
2012-06-24 12:02:52 +0100

    Simple stubbed JSON responses for /info, /stats and /health

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ healthcheck-stub
 ================
 
 Ratpack stub for a notional application-healthcheck REST service
+
+Run as gradlew jettyRunWar
--- src/app/resources/scripts/app.groovy
+++ src/app/resources/scripts/app.groovy
@@ -1,3 +1,23 @@
-get('/') {
-   render('index.html')
+get('/health') {
+    setHeader("Content-Type", "application/json")
+    """{
+    '1' : { 'GREEN': "We apologise for any inconvenience" },
+    '2' : { 'RED': "So this is it then; we're going to die?" }
+}"""
+}
+
+get('/stats') {
+    setHeader("Content-Type", "application/json")
+    """{
+    '1' : { 'Throughput': '10', 'Timeouts' : '1' },
+    '2' : { 'Throughput': '0', 'Timeouts : '100' }
+}"""
+}
+
+get('/info') {
+    setHeader("Content-Type", "application/json")
+    """{
+    '1' : { 'Name' : 'Component A', 'Version': '2.1.0', 'Properties' : {'dbdeploy-changelog-number' : '26'} },
+    '2' : { 'Name' : 'Component B', 'Version': '1.0.1-SNAPSHOT', 'Properties : {'dbdeploy-changelog-number' : '37'} }
+}"""
 }
