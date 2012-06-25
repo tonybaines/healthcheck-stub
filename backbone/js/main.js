a0cf778cfdf150c6179325d5bd42ad8879ae4fe7
Tony Baines <tony.baines.1@gmail.com>
2012-06-25 16:16:33 +0100

    Working GET/render for the health-check data

--- backbone/js/main.js
+++ backbone/js/main.js
@@ -1,13 +1,8 @@
-// Author: Thomas Davis <thomasalwyndavis@gmail.com>
-// Filename: main.js
-
-// Require.js allows us to configure shortcut alias
-// Their usage will become more apparent futher along in the tutorial.
 require.config({
   paths: {
     jquery: 'libs/jquery/jquery',
     underscore: 'libs/underscore/underscore-min',
-    backbone: 'libs/backbone/backbone',
+    backbone: 'libs/backbone/backbone-optamd3-min',
     text: 'libs/require/text',
     templates: '../templates'
   }
--- src/app/resources/scripts/app.groovy
+++ src/app/resources/scripts/app.groovy
@@ -1,37 +1,30 @@
-//get('/health') {
-//    setHeader("Access-Control-Allow-Origin", "*");
-//    setHeader("Access-Control-Allow-Headers", "X-Requested-With");
-//    setHeader("Content-Type", "application/json")
-//    """[
-//    {'id': '1', 'status': { 'level': 'GREEN', 'message': "We apologise for any inconvenience" } },
-//    {'id': '2', 'status': { 'level': 'RED', 'message': "So this is it then; we're going to die?" } }
-//]
-//"""
-//}
-
-get('/health') {
-    setHeader("Access-Control-Allow-Origin", "*");
-    setHeader("Access-Control-Allow-Headers", "X-Requested-With");
-    setHeader("Content-Type", "application/json")
-    "[ {'id': '1'}, {'id': '2'} ]"
-}
-
-get('/stats') {
+get("/health") {
     setHeader("Access-Control-Allow-Origin", "*");
     setHeader("Access-Control-Allow-Headers", "X-Requested-With");
     setHeader("Content-Type", "application/json")
     """[
-    {'id': '1', 'stats': { 'Throughput': '10', 'Timeouts' : '1' } },
-    {'id': '2', 'stats': { 'Throughput': '0', 'Timeouts : '100' } }
+    {"id": "1", "status": { "level": "GREEN", "message": "We apologise for any inconvenience" } },
+    {"id": "2", "status": { "level": "RED",   "message": "So this is it then; we're going to die?" } }
+]
+"""
+}
+
+get("/stats") {
+    setHeader("Access-Control-Allow-Origin", "*");
+    setHeader("Access-Control-Allow-Headers", "X-Requested-With");
+    setHeader("Content-Type", "application/json")
+    """[
+    {"id": "1", "stats": { "Throughput": "10", "Timeouts" : "1" } },
+    {"id": "2", "stats": { "Throughput": "0", "Timeouts : "100" } }
 ]"""
 }
 
-get('/info') {
+get("/info") {
     setHeader("Access-Control-Allow-Origin", "*");
     setHeader("Access-Control-Allow-Headers", "X-Requested-With");
     setHeader("Content-Type", "application/json")
     """[
-    {'id': '1', 'info': { 'name' : 'Component A', 'version': '2.1.0', 'properties' : {'dbdeploy-changelog-number' : '26'} } },
-    {'id': '2', 'info': { 'name' : 'Component B', 'version': '1.0.1-SNAPSHOT', 'properties : {'dbdeploy-changelog-number' : '37'} } }
+    {"id": "1", "info": { "name" : "Component A", "version": "2.1.0", "properties" : {"dbdeploy-changelog-number" : "26"} } },
+    {"id": "2", "info": { "name" : "Component B", "version": "1.0.1-SNAPSHOT", "properties : {"dbdeploy-changelog-number" : "37"} } }
 ]"""
 }
